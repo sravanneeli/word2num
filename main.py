e2dc7e9cc25315fdb4478905d3f65739643970c5
sravanneeli <46817445+sravanneeli@users.noreply.github.com>
2019-10-27 11:46:34 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,6 @@
+import module_1 as w2n
+
+if __name__ == '__main__':
+    a = w2n.process_input('''once i gave a person five thousand dollars and he has to give to me "Triple A"''')
+    new_para = a.word2num()
+    print(new_para)
